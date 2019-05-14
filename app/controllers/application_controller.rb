09eb2494fcdf207e61177b1ebf33d47344c9eb9f
Jerald <darthj93@gmail.com>
2019-05-13 19:59:59 -0500

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+	def nearby(lng1, lat1, lng2, lat2)
+	    if (lng1-lng2).abs <= 0.01 && (lat1-lat2).abs <= 0.01
+	      return true
+	    else
+	      return false
+	    end
+    end
+end
